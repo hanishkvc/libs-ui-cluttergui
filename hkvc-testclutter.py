4813162ee33c9d808ef22751e08e6dddd0a2b292
hanishkvc <hanishkvc@gmail.com>
2020-11-24 11:29:06 +0530

    Handle events
    As main_quit takes no arguments and the callback registered
    for stage destroy event through stage.connect, will recieve the
    actor ie stage itself as a argument to it, so a inbetween callback
    handle_destroy has been added.
    
    Also added a mouse click handler, which quits the app, if the
    actor on which mouse was clicked is the stage.

--- hkvc-testclutter.py
+++ hkvc-testclutter.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python3
+# Test Clutter
+# HanishKVC, v20201124IST1127
+#
 from gi.repository import Clutter
+
 
 # Initialise
 Clutter.init()
+
 
 # Create the stage
 stage = Clutter.Stage()
@@ -9,9 +15,23 @@ stage.set_background_color(Clutter.color_from_string("Red")[1])
 stage.set_size(400,400)
 stage.set_title("Hello World 7")
 
+
+# Handle events
+def handle_btn_press(actor, event):
+    print("INFO:BtnPress:{},{}".format(actor, event))
+    if actor == stage:
+        Clutter.main_quit()
+
+
+def handle_destroy(actor):
+    print("INFO:destroy:WhyDear-OkOk:{}".format(actor))
+    Clutter.main_quit()
+
+
 # Get ready to start
+stage.connect("destroy", handle_destroy)
+stage.connect("button-press-event", handle_btn_press)
 stage.show_all()
 Clutter.main()
-
 
 
