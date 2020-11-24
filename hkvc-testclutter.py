0cbd4e09b6421b2bd970fdac378156d3413bf1ca
hanishkvc <hanishkvc@gmail.com>
2020-11-24 11:38:02 +0530

    Show a Text label
    A a non editable text label. Do note that the Text object can be
    made editable by calling set_editable on it.
    
    One can specify color in clutter using any one of the following
    
    a) color_from_string
    
    	a.1) a color name like "black"
    
    	a.2) a hex string like "#RRGGBBAA"
    
    	as noted before color_from_string returns success/failure
    	and the color object in a tuple, so get the color object
    	by indexing into position 1 in the tuple (bcas 0 position
    	contains true/false related to success or not).
    
    b) color_from_pixel where the argument is a 32bit integer containing
    the color value like 0xRRGGBBAA
    
    c) Clutter.Color.new(r,g,b,a)

--- hkvc-testclutter.py
+++ hkvc-testclutter.py
@@ -12,8 +12,19 @@ Clutter.init()
 # Create the stage
 stage = Clutter.Stage()
 stage.set_background_color(Clutter.color_from_string("Red")[1])
-stage.set_size(400,400)
+stage.set_size(600,400)
 stage.set_title("Hello World 7")
+
+
+# Create children
+label = Clutter.Text()
+label.set_text("Hello again 007")
+label.set_color(Clutter.Color.new(0x00, 0x00, 0xff, 0xff))
+label.set_color(Clutter.color_from_string("#0000ff80")[1])
+label.set_color(Clutter.color_from_pixel(0x00ff0080))
+label.set_font_name("Mono 32")
+label.set_position(200, 200)
+stage.add_child(label)
 
 
 # Handle events
