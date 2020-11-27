019045a97e89e8e2ebe30d664c6fb086c4492f35
hanishkvc <hanishkvc@gmail.com>
2020-11-28 03:51:26 +0530

    Get size used by a text
    Either by asking the font or by looking at rendered surface.

--- hkvc-genimages.py
+++ hkvc-genimages.py
@@ -14,7 +14,10 @@ f=pygame.sysfont.SysFont(None,32)
 
 # update the surface
 s.fill((128,128,128))
+text = "testme"
+print(f.size(text))
 fs=f.render("testme",True,(0,0,100))
+print(fs.get_size())
 s.blit(fs,(20,20))
 
 # Show the surface to user
