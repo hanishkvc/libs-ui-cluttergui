27de85d1998a1c1b39db1270dd41bd7688fa6514
hanishkvc <hanishkvc@gmail.com>
2020-11-28 03:39:14 +0530

    Use pygame to generate test images P1
    A basic partial skeletal pygame

---
+++ hkvc-genimages.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Generate some test images
+# HanishKVC, 2020
+
+import pygame
+
+# Setup a surface
+s=pygame.display.set_mode((640,480))
+
+# Get a font
+pygame.font.init()
+f=pygame.sysfont.SysFont(None,32)
+
+# update the surface
+s.fill((128,128,128))
+fs=f.render("testme",True,(0,0,100))
+s.blit(fs,(300,200))
+
+# Show the surface to user
+pygame.display.update()
+
+# Save the surface for user
+pygame.image.save(s,"/tmp/testme.png")
+
