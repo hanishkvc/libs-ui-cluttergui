28660455fb75826c70ef8d641dba68064abe45dd
hanishkvc <hanishkvc@gmail.com>
2020-11-28 03:43:59 +0530

    A off-screen surface P2

--- hkvc-genimages.py
+++ hkvc-genimages.py
@@ -5,7 +5,8 @@
 import pygame
 
 # Setup a surface
-s=pygame.display.set_mode((640,480))
+#s=pygame.display.set_mode((640,480))
+s=pygame.Surface((128,128))
 
 # Get a font
 pygame.font.init()
@@ -14,10 +15,10 @@ f=pygame.sysfont.SysFont(None,32)
 # update the surface
 s.fill((128,128,128))
 fs=f.render("testme",True,(0,0,100))
-s.blit(fs,(300,200))
+s.blit(fs,(20,20))
 
 # Show the surface to user
-pygame.display.update()
+#pygame.display.update()
 
 # Save the surface for user
 pygame.image.save(s,"/tmp/testme.png")
