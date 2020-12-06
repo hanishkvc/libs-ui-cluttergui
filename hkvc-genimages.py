1630f2e14de9465aad7f2262a82ff70953f251ad
hanishkvc <hanishkvc@gmail.com>
2020-12-07 02:36:20 +0530

    GenImages: Controllable fillcolor, including alpha if required
    For the alpha value in fillColor to take effect, the surface used
    should have the alpha channel.
    
    Cat images have been made partially transparent
    
    Audio nav control btn images have been made lot more transparent

--- hkvc-genimages.py
+++ hkvc-genimages.py
@@ -7,6 +7,7 @@ import pygame
 # Setup a surface
 #s=pygame.display.set_mode((640,480))
 s128=pygame.Surface((128,128))
+s128a=pygame.Surface((128,128), pygame.SRCALPHA)
 s64=pygame.Surface((64,64))
 s64a=pygame.Surface((64,64), pygame.SRCALPHA)
 s736x64=pygame.Surface((736,64))
@@ -18,10 +19,10 @@ pygame.font.init()
 f=pygame.sysfont.SysFont(None,32)
 
 
-def gen_image(s, text, fileName):
+def gen_image(s, text, fileName, fillColor=(128,128,128,128)):
     print(text, fileName)
     surfaceSize = s.get_size()
-    s.fill((128,128,128,64))
+    s.fill(fillColor)
     fs=f.render(text, True, (0,0,100))
     textSize = fs.get_size()
     x = (surfaceSize[0] - textSize[0])/2
@@ -41,14 +42,17 @@ for g in groups:
     for i in range(groupMemberCnt):
         text = "{}{}".format(g, i)
         fileName = "{}.png".format(text)
-        gen_image(s128, text, fileName)
+        if g == "Cat":
+            gen_image(s128a, text, fileName)
+        else:
+            gen_image(s128, text, fileName)
 
 # Media Navigation control
 
 controls = [ "Play", "Pause", "Next", "Prev", "Vol+", "Vol-", "Bri+", "Bri-", "Back" ]
 for control in controls:
     fileName = control + ".png"
-    gen_image(s64a, control, fileName)
+    gen_image(s64a, control, fileName, fillColor=(128,128,128,64))
 
 aitems = [ "aitem1", "aitem2", "aitem3", "aitem4", "aitem5", "aitem6", "aitem7", "aitem8", "aitem9" ]
 for aitem in aitems:
