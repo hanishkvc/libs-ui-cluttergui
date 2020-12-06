3b98f4575205aa8b022c7cf73f3ba95435338376
hanishkvc <hanishkvc@gmail.com>
2020-12-07 05:13:48 +0530

    CM: Video playback initial skeleton
    When using the ClutterGst's builtin Playback logic, In this case
    compared to audio, I require to additionally create a CGST Content
    and set the Playback entity to it. Inturn I require to set the
    content into a existing UI element on the screen.

--- cluttermedia.py
+++ cluttermedia.py
@@ -14,7 +14,7 @@ gPlayer = None
 def play_audio(sFile):
     global gPlayer
     stop()
-    print("INFO:CM:Play", sFile)
+    print("INFO:CM:PlayA", sFile)
     gPlayer = ClutterGst.Playback()
     gPlayer.set_filename(sFile)
     gPlayer.set_audio_volume(0.6)
@@ -26,4 +26,18 @@ def stop():
         print("INFO:CM:Stop in case")
         gPlayer.set_playing(False)
 
+
+def play_video(sFile, ui):
+    global gPlayer
+    stop()
+    print("INFO:CM:PlayV", sFile)
+    gPlayer = ClutterGst.Playback()
+    gPlayer.set_filename(sFile)
+    gPlayer.set_audio_volume(0.6)
+    cgstContent = ClutterGst.Aspectratio()
+    cgstContent.set_player(gPlayer)
+    ui.set_content(cgstContent)
+    # default content gravity is resize fill, so things should be fine.
+    # need to check, if I require to set the scaling filters explicitly or default is good enough.
+    gPlayer.set_playing(True)
 
